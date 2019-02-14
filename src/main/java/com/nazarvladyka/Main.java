faf67730b5d5a42908023623ac2bfe6b05015b64
Nazar Vladyka <nazarvladyka00@gmail.com>
2019-02-15 00:13:46 +0200

    do all tasks

---
+++ src/main/java/com/nazarvladyka/Main.java
@@ -0,0 +1,10 @@
+package com.nazarvladyka;
+
+import com.google.common.collect.HashMultiset;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        HashMultiset hashMultiset = HashMultiset.create(); //check adding Guava dependency
+    }
+}
